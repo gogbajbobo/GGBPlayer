640bc5a18c04c714a56de523962d410c67d0a2b3
gogbajbobo <grigoriev.maxim@gmail.com>
2017-10-16 14:37:55 +0300

    Initial Commit

---
+++ GGBPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGBPlayer
+//
+//  Created by Maxim Grigoriev on 16/10/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GGBPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GGBPlayer
+//
+//  Created by Maxim Grigoriev on 16/10/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
