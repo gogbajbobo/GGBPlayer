be7872ad147105c0b108066740a0585d1c796d36
gogbajbobo <grigoriev.maxim@gmail.com>
2017-10-23 12:32:40 +0300

    add GGBConstants.h

---
+++ GGBPlayer/GGBConstants.h
@@ -0,0 +1,15 @@
+//
+//  GGBConstants.h
+//  GGBPlayer
+//
+//  Created by Maxim Grigoriev on 23/10/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#ifndef GGBConstants_h
+#define GGBConstants_h
+
+#define CELL_HEIGHT 56.0
+
+
+#endif /* GGBConstants_h */
