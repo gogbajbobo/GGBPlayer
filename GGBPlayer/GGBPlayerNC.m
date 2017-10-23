185e4159ccd06f09f4530222778f7347eda3cff8
gogbajbobo <grigoriev.maxim@gmail.com>
2017-10-23 12:33:17 +0300

    add GGBPlayerNC

---
+++ GGBPlayer/GGBPlayerNC.m
@@ -0,0 +1,37 @@
+//
+//  GGBPlayerNC.m
+//  GGBPlayer
+//
+//  Created by Maxim Grigoriev on 23/10/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#import "GGBPlayerNC.h"
+#import "GGBLibraryController.h"
+
+
+@interface GGBPlayerNC ()
+
+@end
+
+
+@implementation GGBPlayerNC
+
+- (void)customInit {
+    [GGBLibraryController start];
+}
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    [self customInit];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
